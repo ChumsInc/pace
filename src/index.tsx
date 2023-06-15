afb89caa59e04bf87282c0dbaa77f18683bc2eec
Steve Montgomery <steve@chums.com>
2023-06-15 16:16:45 -0600

    chore: remove unused import

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import {Provider} from 'react-redux';
 import {HashRouter as Router} from 'react-router-dom';
 import App from './app/App';
 import store from './app/configureStore'
-import CssBaseline from "@mui/material/CssBaseline";
 
 window.localStorage.setItem('debug', '*');
 const container = document.getElementById('app');
@@ -14,7 +13,6 @@ root.render(
     <React.StrictMode>
         <Provider store={store}>
             <Router>
-
                 <App/>
             </Router>
         </Provider>
