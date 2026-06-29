d9bd8760af0150642e651446f71dcf8def114551
Steve Montgomery <steve@chums.com>
2026-06-29 13:52:26 -0600

    refactor: replace Offcanvas with Toast for version update notification

--- src/components/version/VersionUpdateToast.tsx
+++ src/components/version/VersionUpdateToast.tsx
@@ -1,5 +1,5 @@
 import {useVersion} from "@/components/version/useVersion.ts";
-import {Offcanvas} from "react-bootstrap";
+import {Toast, ToastContainer} from "react-bootstrap";
 import {startTransition, useEffect, useState} from "react";
 
 export default function VersionUpdateToast() {
@@ -18,13 +18,17 @@ export default function VersionUpdateToast() {
     }
 
     return (
-        <Offcanvas show={show} onHide={closeHandler} placement="bottom">
-            <Offcanvas.Header closeButton>
-                <Offcanvas.Title>Update Available</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-                A new version is available. Please refresh the page to update.
-            </Offcanvas.Body>
-        </Offcanvas>
+        <ToastContainer position="bottom-end">
+            <Toast show={show} onClose={closeHandler} bg="warning" className="text-dark">
+                <Toast.Header closeButton>
+                    <strong className="me-auto">
+                        Update Available
+                    </strong>
+                </Toast.Header>
+                <Toast.Body>
+                    A new version is available. Please refresh the page to update.
+                </Toast.Body>
+            </Toast>
+        </ToastContainer>
     )
 }
